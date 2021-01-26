6ca84b57cce3b37e946c8d2e6fb773232df92562
serhiistr <sergaudit2019@gmail.com>
2021-01-26 13:47:51 +0200

    26012021

---
+++ task 4.py
@@ -0,0 +1,17 @@
+# Что выведет код?
+
+x = True
+y = False
+z = False
+
+if not x or y:                          #not X = False и Y = False
+    print(1)
+elif not x or not y and z:              # not X = False или (not Y = True и z = False)
+    print(2)
+elif not x or y or not y and x:         # (not X = False) или (Y = False) или (not Y = True и X = True)
+    print(3)
+else:
+    print(4)
+
+# Стоит помнить, что NOT имеет наивысший приоритет, потом идет AND, затем OR
+# Выйдет из цикла когда будет True в результате
