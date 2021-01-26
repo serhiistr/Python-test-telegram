c686b09927ff44b46790d85070944d677e69e4bf
serhiistr <sergaudit2019@gmail.com>
2021-01-26 18:53:37 +0200

    26012021

---
+++ task 11.py
@@ -0,0 +1,13 @@
+# Что выведет код?
+
+
+class Person:
+    def __init__(self, n, s):
+        self.name = n
+        self.surname = s
+
+p1 = Person("Sam", "Baker")
+
+print(p1.name, p1.surname)
+
+# Ответ - Sam Baker
